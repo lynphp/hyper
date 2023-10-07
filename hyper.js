53c84247f37e49f5851fa1fcb755b5227795ab79
resti.guay <git.lynphp@gmail.com>
2023-10-07 22:45:26 +0800

    add form handling

--- hyper.js
+++ hyper.js
@@ -1,7 +1,7 @@
 const hyper =((directive='hyper')=>{
-    const selector = '*['+directive +']';
-    const pending=[];
-    const pages=[];
+    const selector = '*['+directive +']'
+    const pending=[]
+    const pages=[]
     var uid = function(i) {
         return function () {
             return 'uid' + (++i)
@@ -9,7 +9,7 @@ const hyper =((directive='hyper')=>{
     }(0);
 
     function setHID(element){
-        let hid = '';
+        let hid = ''
         if (!element.hasAttribute('id')) {
             hid = uid();
             element.setAttribute('id',hid)
@@ -18,14 +18,17 @@ const hyper =((directive='hyper')=>{
     }
 
     function getURL(element){
-        let params=getDataAttribute(element);
+        let params= getDataAttribute(element)
         if(element.hasAttribute('fetch')){
             return element.getAttribute('fetch').split(":")[1]+'?'+params
-        }else if(element.nodeName.toLowerCase()==='a'){
+        }else if(element.nodeName === 'A'){
             if(element.getAttribute('href').startsWith('javascript')){
-                return  undefined;
+                eval(element.getAttribute('href'))
+                return  undefined
             }
             return element.getAttribute('href')+'?'+params;
+        }else if(element.nodeName==='FORM' && element.hasAttribute('action')){
+            return element.getAttribute('action')
         }
     }
 
@@ -33,38 +36,41 @@ const hyper =((directive='hyper')=>{
         const queryParams = {}
         Object.getOwnPropertyNames(element.dataset).forEach((prop)=>{
             if(element.dataset[prop].startsWith("bind")){
-                queryParams[prop]=document.querySelector(element.dataset[prop].split(':')[1])?.value
+                queryParams[prop] = document.querySelector(element.dataset[prop].split(':')[1])?.value
             }else{
-                queryParams[prop]=element.dataset[prop]
+                queryParams[prop] = element.dataset[prop]
             }
         });
-        return (new URLSearchParams(queryParams)).toString();
+        return (new URLSearchParams(queryParams)).toString()
     }
+
     function get(element) {
         let url = getURL(element)
-        if(url!==undefined) {
+        if(url !== undefined) {
+
             if (element.hasAttribute('static') && pages[url] !== undefined) {
                 return pages[url];
             }
             function getAndWait(url) {
-                const getRequest = new Request(url, {
+                const getRequest= new Request(url, {
                     method: "GET",
                     mode: "cors",
                     headers: {'Accept': 'text/html'}
                 });
                 return fetch(getRequest).then(function (response) {
-
-                    let html = response.text();
-                    return html;
+                    let html= response.text();
+                    return html
                 }).catch(function (err) {
-                    console.log('Failed to fetch page: ', err);
-                    return '';
+                    console.log('Failed to fetch page: ', err)
+                    return ''
                 });
             }
-            let response = getAndWait(url);
+
+            let response = getAndWait(url)
             if (element.hasAttribute('static') && pages[url] === undefined) {
                 pages[url] = response;
             }
+
             return response;
         } else {
             return '';
@@ -73,25 +79,23 @@ const hyper =((directive='hyper')=>{
 
     function post(element){
         let url = getURL(element)
-        if(url!==undefined) {
+        if(url !== undefined) {
             if (element.hasAttribute('static') && pages[url] !== undefined) {
-                return pages[url];
+                return pages[url]
             }
-
-            function postAndWait() {
-                const getRequest = new Request(url, {
+            function postAndWait(data) {
+                const getRequest= new Request(url, {
                     method: "POST",
                     mode: "cors",
-                    headers: {'Accept': 'text/html'}
+                    headers: {'Accept': 'text/html'},
                 });
                 return fetch(getRequest).then(function (response) {
                     return response.text();
                 }).catch(function (err) {
-                    console.log('Failed to fetch page: ', err);
+                    console.log('Failed to fetch page: ', err)
                 });
             }
-
-            let response = postAndWait();
+            let response= postAndWait()
             if (element.hasAttribute('static') && pages[url] === undefined) {
                 pages[url] = response;
             }
@@ -105,9 +109,9 @@ const hyper =((directive='hyper')=>{
         const queryParams = {}
         Object.getOwnPropertyNames(element.dataset).forEach((prop)=>{
             if(element.dataset[prop].startsWith("bind")){
-                queryParams[prop]=document.querySelector(element.dataset[prop].split(':')[1])?.value
+                queryParams[prop] = document.querySelector(element.dataset[prop].split(':')[1])?.value
             }else{
-                queryParams[prop]=element.dataset[prop]
+                queryParams[prop] = element.dataset[prop]
             }
         });
         return (new URLSearchParams(queryParams)).toString();
@@ -120,7 +124,7 @@ const hyper =((directive='hyper')=>{
     }
 
     function replace(content, element){
-        if(element.getAttribute('replace')!=='self'){
+        if(element.getAttribute('replace') !== 'self'){
             element.replaceWith(content);
         }else{
             document.querySelector(element.getAttribute('replace')).replace(content);
@@ -128,31 +132,35 @@ const hyper =((directive='hyper')=>{
             handle(document.querySelector(element.getAttribute('replace')))
         }
     }
+
     function initElementIDs(){
         document.querySelectorAll(selector).forEach((elem)=>{
             setHID(elem)
         })
     }
+
     function handleChunk(html){
         let doc = new DOMParser().parseFromString(html,'text/html')
         doc.querySelectorAll('*[id]').forEach(async(elem)=>{
             await handle(document.getElementById(elem.id))
         })
     }
+
     function fill(content, element){
 
         if (element.getAttribute('fill') !== 'self') {
-            document.querySelector(document.querySelector(element.getAttribute('fill'))).innerHTML = content;
+            document.querySelector(element.getAttribute('fill')).innerHTML = content;
             initElementIDs();
-            handleChunk(document.querySelector(document.querySelector(element.getAttribute('fill'))).innerHTML);
+            handleChunk(document.querySelector(element.getAttribute('fill')).innerHTML);
         } else {
             element.innerHTML = content
             initElementIDs();
             handleChunk(element.innerHTML);
         }
     }
+
     function populate(content, element){
-        if(element.nodeName==='A') {
+        if(element.nodeName === 'A') {
             if(element.hasAttribute('fill')){
                 fill(content, element)
             }
@@ -162,7 +170,7 @@ const hyper =((directive='hyper')=>{
             fill(content, element)
         }else {
             element.innerHTML = content
-            initElementIDs();
+            initElementIDs()
             handleChunk(element.innerHTML);
         }
 
@@ -172,60 +180,106 @@ const hyper =((directive='hyper')=>{
         let response = await fetchContent(element)
         populate(response, element)
     }
+
     function getHID(element){
         return element.getAttribute('hid')
     }
+
     async function fetchContent(element){
         if(element.getAttribute('fetch')?.startsWith('GET')){
-            return get(element);
+            return get(element)
         }
         if(element.getAttribute('fetch')?.startsWith('POST')){
-            return  post(element);
+            return  post(element)
         }
-        return get(element);
+        return get(element)
     }
 
     function debounce(func, timeout = 300){
-        let timer;
+        let timer
         return (...args) => {
             clearTimeout(timer);
             timer = setTimeout(() => { func.apply(this, args); }, timeout);
         };
     }
+
+    async function submitForm(element){
+        let url = getURL(element)
+        let data= {}
+        if(url !== undefined) {
+            if (element.hasAttribute('static') && pages[url] !== undefined) {
+                return pages[url]
+            }
+            async function postAndWait(data) {
+                const getRequest= new Request(url, {
+                    method: element.getAttribute('method'),
+                    mode: "cors",
+                    headers: {'Accept': 'text/html'},
+                    body: data,
+                });
+                return fetch(getRequest).then(function  (response) {
+                    return response.text();
+                }).catch(function (err) {
+                    console.log('Failed to fetch page: ', err)
+                });
+            }
+            if(element.nodeName==='FORM'){
+                data = new FormData(element)
+            }
+            let response= await postAndWait()
+            if (element.hasAttribute('static') && pages[url] === undefined) {
+                pages[url] = response
+            }
+            populate(response,element)
+        }else{
+            return ''
+        }
+    }
+
     async function handle(element){
+        if(!element.hasAttribute(directive)){
+            return;
+        }
         setHID(element)
-        if(element.hasAttribute('listen')){
-            let listenParts= element.getAttribute('listen').toLowerCase().split(':');
-            let elem = document.querySelector(listenParts[0]);
+
+        if (element.hasAttribute('listen')) {
+            let listenParts= element.getAttribute('listen').toLowerCase().split(':')
+            let elem = document.querySelector(listenParts[0])
             elem.addEventListener(listenParts[1], ()=>{
                  executeFetch(element)
             })
-        }else if(element.hasAttribute('trigger')){
-            let on = element.getAttribute('trigger').toLowerCase().split(',');
-            on.forEach((event) =>{
-                if( event ==='load'){
+        } else if (element.hasAttribute('trigger')) {
+            let on= element.getAttribute('trigger').toLowerCase().split(',')
+            on.forEach((event) => {
+                if ( event === 'load') {
                      executeFetch(element)
-                } else if ('on' + event in element){
+                } else if ('on' + event in element) {
                     element.addEventListener(event,async (e)=>{await executeFetch(element)})
                 }
             })
-        }else if(element.hasAttribute('prepare')) {
+        } else if (element.hasAttribute('prepare')) {
             element.addEventListener(element.getAttribute('prepare'), debounce(async () => {
-                if(pending[element]===undefined) {
-                    pending[element]=true;
-                    pages[getURL(element)] = await fetchContent(element);
-                    let dom = new DOMParser().parseFromString(pages[getURL(element)], 'text/html')
-                    let elements = dom.querySelectorAll('*[hyper]')
+                if(pending[element] === undefined) {
+                    pending[element] = true
+                    pages[getURL(element)] = await fetchContent(element)
+                    let dom= new DOMParser().parseFromString(pages[getURL(element)], 'text/html')
+                    let elements= dom.querySelectorAll('*[hyper]')
                     elements.forEach((element)=> {
-                        handle(element);
-                    });
-                    removePending(element);
+                        handle(element)
+                    })
+                    removePending(element)
                 }
             }));
-        }else if(element.hasAttribute('static')){
+        } else if (element.hasAttribute('static')) {
             executeFetch(element)
+        } else if (element.hasAttribute('action')) {
+            element.addEventListener('submit',(event)=>{
+                event.preventDefault()
+                submitForm(event.target)
+            })
         }
     }
+
     document.querySelectorAll(selector).forEach((element)=> {
        handle(element);
     });
